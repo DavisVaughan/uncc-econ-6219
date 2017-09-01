12a54885395d2f59a9d95fcc0dd1cdabcf4cfb21
DavisVaughan <mvaugh15@uncc.edu>
2017-09-01 14:30:53 -0400

    setup for assignment 1

---
+++ assignments/hw-01/work/hw_01.m
@@ -0,0 +1,13 @@
+%%% DETAILS
+
+% Author - Davis Vaughan
+% Date   - 2017-09-01
+% HW     - 01
+
+% If you have opened the simulink project, your path should be set with a
+% working directory set to the `work` folder so the relative references
+% should just work
+
+%%% LOAD DATA
+
+crsp_returns = xlsread('../data/sizeDecileReturns.xls');
